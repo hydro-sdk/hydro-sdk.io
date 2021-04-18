359602b75f45de094e4ac8009bd6b7e1b3794d37
Chris Gibb <chris.gibb@outlook.com>
2021-04-17 23:05:10 -0400

    add code sample

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -5,7 +5,7 @@ import styles from './HomepageFeatures.module.css';
 const FeatureList = [
   {
     title: 'Easy to Use',
-    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
+    png: "img/hello-world-carbon.png",
     description: (
       <>
         Docusaurus was designed from the ground up to be easily installed and
@@ -15,7 +15,7 @@ const FeatureList = [
   },
   {
     title: 'Focus on What Matters',
-    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
+    png: "",
     description: (
       <>
         Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
@@ -25,7 +25,7 @@ const FeatureList = [
   },
   {
     title: 'Powered by React',
-    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
+    png: "",
     description: (
       <>
         Extend or customize your website layout by reusing React. Docusaurus can
@@ -35,11 +35,11 @@ const FeatureList = [
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({png, title, description}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} alt={title} />
+        <img  src={png}  alt={title} />
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
